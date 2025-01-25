e22956ab7ca04c22b64b2da3a3841b48aece910c
JacobCormier <jacob.cormier1343@gmail.com>
2025-01-24 21:44:26 -0400

    Created a level1 scene with the base globe

--- scripts/test_scene.gd
+++ scripts/test_scene.gd
@@ -1,1 +1,18 @@
 extends Node3D
+
+
+
+func _ready() -> void:
+	pass
+
+func random_on_sphere(radius : float) -> Vector3:
+   # Generate random spherical coordinates
+	var theta = 2 * PI * randf()
+	var phi = PI * randf()
+   
+   # Convert to cartesiana
+	var x = sin(phi) * cos(theta) * radius
+	var y = sin(phi) * sin(theta) * radius
+	var z = cos(phi) * radius
+   
+	return Vector3(x,y,z)
