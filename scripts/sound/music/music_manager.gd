2f93e21548aa90f0932006c793642487430bb8ce
JacobCormier <jacob.cormier1343@gmail.com>
2025-01-25 16:19:23 -0400

    Player Collider

--- scripts/sound/music/music_manager.gd
+++ scripts/sound/music/music_manager.gd
@@ -12,12 +12,14 @@ func _ready():
 	# Create an AudioStreamPlayer node dynamically or ensure it exists in the scene tree
 	music_player = AudioStreamPlayer.new()
 	music_player.pitch_scale = 1.0
+	music_player.volume_db = -20.0
 	add_child(music_player)
 	play_music_sequence()
 
 func play_music_sequence():
 	music_player.stream = THEME_INTRO_AND_VERSE
 	music_player.play()
+	print(music_player.volume_db)
 	music_player.finished.connect(_on_theme_intro_finished)
 
 func _on_theme_intro_finished():
