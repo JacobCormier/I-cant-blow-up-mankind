07fde814bfd4e665f67f4709361dc46ab98652a9
JacobCormier <jacob.cormier1343@gmail.com>
2025-01-26 18:12:43 -0400

    Added a dialogue box

---
+++ scenes/ui/dialogue_script.gd
@@ -0,0 +1,29 @@
+extends Control
+
+@onready var dialogue_label: Label = $NinePatchRect/MarginContainer/DialogueLabel
+
+@export var letter_reveal_speed = 16.0
+@export var dissapear_delay = 40
+
+var is_triggered = false
+var letters_count = 0
+
+func _ready() -> void:
+	dialogue_label.visible_characters = 0
+	self.visible = false
+
+func _process(delta: float) -> void:
+	
+	if is_triggered:
+		letters_count += delta * letter_reveal_speed
+		var int_letter_count = letters_count as int
+		
+		if int_letter_count > dialogue_label.visible_characters and dialogue_label.visible_characters < dialogue_label.get_total_character_count():
+			dialogue_label.visible_characters = int_letter_count
+			
+		if int_letter_count > dialogue_label.get_total_character_count() + dissapear_delay:
+			self.queue_free()
+
+func trigger_dialogue() -> void:
+	is_triggered = true
+	self.visible = true
