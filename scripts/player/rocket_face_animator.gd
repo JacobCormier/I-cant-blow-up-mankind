ec0eb2720a15837e3b4ca7506cc3fcd550566ee5
JacobCormier <jacob.cormier1343@gmail.com>
2025-01-26 19:43:56 -0400

    changed talking frequency

--- scripts/player/rocket_face_animator.gd
+++ scripts/player/rocket_face_animator.gd
@@ -1,5 +1,7 @@
 extends Node3D
 
+enum FaceStates {NONE, TALK, SURPRISE, PANIC, SAD, HAPPY, EXCITED}
+
 @onready var mouth: MeshInstance3D = $FakeBlock/Mouth
 
 const MAX_MOUTH_HEIGHT = 0.45
@@ -7,6 +9,8 @@ const MIN_MOUTH_HEIGHT = 0.1
 
 # The time for the mouth to open and close
 var talk_frequency = 1.0
+const MIN_TALK_FREQUENCY = 0.1
+const MAX_TALK_FREQUENCY = 0.6
 
 var tween: Tween
 
@@ -16,7 +20,7 @@ func _ready() -> void:
 
 func randomize_talk_frequency() -> void:
 	tween = null
-	talk_frequency = randf_range(0.2, 2.0)
+	talk_frequency = randf_range(MIN_TALK_FREQUENCY, MAX_TALK_FREQUENCY)
 	tween = get_tree().create_tween()
 	tween.tween_property(mouth, "mesh:height", MIN_MOUTH_HEIGHT, talk_frequency/2.0)
 	tween.tween_property(mouth, "mesh:height", MAX_MOUTH_HEIGHT, talk_frequency/2.0)
