579736a572e308a98d793b191b52fb35e5113210
BuildTools <unconfigured@null.spigotmc.org>
2025-01-26 23:26:19 -0400

    Added rocket face overlay and glitch shader

--- scripts/player/rocket_face_animator.gd
+++ scripts/player/rocket_face_animator.gd
@@ -2,7 +2,8 @@ extends Node3D
 
 enum FaceStates {NONE, TALK, SURPRISE, PANIC, SAD, HAPPY, EXCITED}
 
-@onready var mouth: MeshInstance3D = $FakeBlock/Mouth
+@onready var camera_3d: Camera3D = $Camera3D
+@onready var mouth: MeshInstance3D = $MenuPlayer/FakeBlock/Mouth
 
 const MAX_MOUTH_HEIGHT = 0.45
 const MIN_MOUTH_HEIGHT = 0.1
@@ -13,9 +14,17 @@ const MIN_TALK_FREQUENCY = 0.1
 const MAX_TALK_FREQUENCY = 0.6
 
 var tween: Tween
+var camera_root_position
+var last_jitter = [0, 0, 0]
 
 func _ready() -> void:
 	randomize_talk_frequency()
+	camera_root_position = camera_3d.position
+	
+func _process(delta):
+	micro_rumble(camera_3d, camera_root_position, delta, 0)
+	micro_rumble(camera_3d, camera_root_position, delta, 1, 0.02, 0.02, 0.07)
+	micro_rumble(camera_3d, camera_root_position, delta, 2, 0.05, 0.05, 0.13)
 
 
 func randomize_talk_frequency() -> void:
@@ -25,4 +34,13 @@ func randomize_talk_frequency() -> void:
 	tween.tween_property(mouth, "mesh:height", MIN_MOUTH_HEIGHT, talk_frequency/2.0)
 	tween.tween_property(mouth, "mesh:height", MAX_MOUTH_HEIGHT, talk_frequency/2.0)
 	tween.finished.connect(randomize_talk_frequency)
+
+func micro_rumble(node, root_position, delta, index, x_variance = 0.01, y_variance = 0.01, time_scale = 0.05):
+	if last_jitter[index] > time_scale:
+		last_jitter[index] = 0
+		var random_rumble_x = root_position.x + randf_range(0, x_variance)
+		var random_rumble_y = root_position.y + randf_range(0, y_variance)
+		node.position = Vector3(random_rumble_x, random_rumble_y, root_position.z)
+	else:
+		last_jitter[index] += delta
 	
--- scripts/ui/game_ui.gd
+++ scripts/ui/game_ui.gd
@@ -5,12 +5,23 @@ extends CanvasLayer
 @onready var score_control: Control = $ScoreControl
 @onready var score_label: Label = $ScoreControl/ScoreLabel
 @onready var fuel_progress_bar: TextureProgressBar = $ScoreControl/FuelProgressBar
+@onready var sub_viewport: SubViewport = $SubViewport
+@onready var texture_rect: TextureRect = $TextureRect
 
 func _ready():
 	death_screen_control.visible = false
 	PlayerStats.on_fuel_changed.connect(update_fuel)
 	PlayerStats.on_score_changed.connect(update_score)
 	PlayerStats.start_gameplay()
+	
+func _process(delta):
+	texture_rect.texture = sub_viewport.get_texture()
+	if texture_rect.material and texture_rect.material.is_class("ShaderMaterial"):
+		var shader_material = texture_rect.material as ShaderMaterial
+		shader_material.set("shader_parameter/time", Time.get_ticks_msec() / 1000.0)
+		shader_material.set("shader_parameter/resolution", Vector2(sub_viewport.size.x, sub_viewport.size.y))
+		shader_material.set("shader_parameter/texture_albedo", sub_viewport.get_texture())
+	
 
 func show_death():
 	death_screen_control.visible = true
