256217f0de93a5b70559110e4117a06d6cdf9829
JacobCormier <jacob.cormier1343@gmail.com>
2025-01-28 09:01:41 -0400

    Added score on fuel pickups

--- scenes/pickups/fuel_pickup_collider.gd
+++ scenes/pickups/fuel_pickup_collider.gd
@@ -1,6 +1,7 @@
 extends Area3D
 
 @export var fuel_amount: int
+@export var score_amount: int
 
 @onready var base_node: Node3D = $"../.."
 
@@ -9,6 +10,7 @@ func _on_area_entered(area: Area3D) -> void:
 	# If they collide with the player:
 	if area.collision_layer == 1:
 		PlayerStats.reload_fuel(fuel_amount)
+		PlayerStats.add_to_score(score_amount)
 		SoundManager.play_pick_up_sound()
 		
 	# If a fuel can is colliding with anything, reset it's position
