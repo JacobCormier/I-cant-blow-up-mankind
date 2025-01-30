e252d0ea578d2cd1eba95ce53e42b605c0e1132e
BuildTools <unconfigured@null.spigotmc.org>
2025-01-30 03:45:28 -0400

    Finished the cutscenes. Full game is working

--- scripts/cutscenes/transition_1_2.gd
+++ scripts/cutscenes/transition_1_2.gd
@@ -1,51 +1,61 @@
 extends Node3D
 
-@onready var game_globe: Node3D = $"../GameGlobe"
 @onready var player: PlayerController = $"../Player"
 @onready var icbm_character: Node3D = $ICBMCharacter
-@onready var follow_cam: Camera3D = $ICBMCharacter/FollowCam
-
-var Transition12Tweens = preload("res://scripts/cutscenes/tweens/transition_1_2_tweens.gd").new()
-
-enum OutroStep {
-	FOLLOW,
-	STATIONARY,
-	INTERCEPT,
-	SPACE,
-	SUN
-}
+@onready var tracking_cam: Camera3D = $ICBMCharacter/TrackingCam
+@onready var transition_globe: Node3D = $TransitionGlobe
+@onready var camera_3d: Camera3D = $Camera3D
 
 var rumble_intensity = 0
-const MAX_RUMBLE_VARIANCE := Vector3(0.25, 0.08, 0)
 
-var step = OutroStep.FOLLOW
 var tween
+var tween2
 var camera_root_position
+var current_ground_color = Color("#60e331")
+var current_sky_color = Color("#38b2f5")
 
+func set_environment_color(ground_color: Color, sky_color: Color) -> void:
+	transition_globe.set_environment_color(ground_color, sky_color)
+	
 func _ready():
 	PlayerStats.is_gameplay_running = false
 	print("Transition")
 	Engine.time_scale = 1
-	camera_root_position = follow_cam.position
+	camera_root_position = tracking_cam.position
 	start_tween_animation(finish)
 	
-func rumble(node, root_position, x_scale = 1, y_scale = 1):
-	var random_rumble_x = root_position.x + randf_range(0, MAX_RUMBLE_VARIANCE.x) * x_scale
-	var random_rumble_y = root_position.y + randf_range(0, MAX_RUMBLE_VARIANCE.y) * y_scale
-	node.position = Vector3(random_rumble_x, random_rumble_y, root_position.z)
+func _process(delta):
+	AnimationUtils.rumble(tracking_cam, camera_root_position, rumble_intensity, rumble_intensity)
+	set_environment_color(current_ground_color, current_sky_color)
 	
 func finish():
 	PlayerStats.is_gameplay_running = true
 	Globals.next_level()
 	
+func switch_camera():
+	camera_3d.make_current()
+	
 func start_tween_animation(callback):
 	tween = get_tree().create_tween()
+	tween2 = get_tree().create_tween()
 	
 	# Shake Camera as rocket flies by
 	tween.set_ease(Tween.EASE_IN)
 	tween.set_trans(Tween.TRANS_SINE)
-	tween.tween_property(self, "rumble_intensity", 4,  3)
-	tween.set_ease(Tween.EASE_OUT)
-	tween.set_trans(Tween.TRANS_SINE)
-	tween.tween_property(self, "rumble_intensity", 0,  5)	
+	tween.tween_property(icbm_character, "position:z", 0,  5)
+	tween.tween_interval(6)
+	tween2.set_trans(Tween.TRANS_LINEAR)
+	tween2.tween_property(tracking_cam, "position:y", -20,  5)
+	tween2.set_ease(Tween.EASE_OUT)
+	tween2.set_trans(Tween.TRANS_BACK)
+	tween2.parallel().tween_property(self, "rumble_intensity", 2,  3)
+	tween2.tween_property(tracking_cam, "position:y", -10,  6)
+	tween2.set_trans(Tween.TRANS_SINE)
+	tween2.parallel().tween_property(tracking_cam, "rotation_degrees:x", 79,  3)
+	tween2.parallel().tween_property(self, "current_sky_color", Color("#0f5ebe"), 3)
+	tween2.parallel().tween_property(self, "current_ground_color", Color("#5b6e54"), 3)
+	tween2.parallel().tween_property(tracking_cam, "position:y", -2000,  8)
+	tween2.parallel().tween_property(icbm_character, "position:z", -2000,  8)
+	tween.tween_interval(2)
+	
 	tween.tween_callback(callback)
--- scripts/cutscenes/transition_2_3.gd
+++ scripts/cutscenes/transition_2_3.gd
@@ -1,85 +1,24 @@
 extends Node3D
-
+@onready var player_origin: Node3D = $PlayerOrigin
 @onready var ground_cam: Camera3D = $GroundCam
-@onready var follow_cam: Camera3D = $ICBMCharacter/FollowCam
 var Transition12Tweens = preload("res://scripts/cutscenes/tweens/transition_1_2_tweens.gd").new()
 
-enum OutroStep {
-	FOLLOW,
-	STATIONARY,
-	INTERCEPT,
-	SPACE,
-	SUN
-}
-
 var rumble_intensity = 0
-const MAX_RUMBLE_VARIANCE := Vector3(0.25, 0.08, 0)
 
-var step = OutroStep.FOLLOW
 var tween
 var camera_root_position
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	PlayerStats.is_gameplay_running = false
-	print("Transition")
+	print("Transition 2 - 3")
 	Engine.time_scale = 1
-	camera_root_position = follow_cam.position
+	SoundManager.kill_sound()
+	camera_root_position = ground_cam.position
 	start_tween_animation(finish)
 	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if step == OutroStep.FOLLOW:
-		print("FOLLOW Follow Cam")
-		rumble(follow_cam, camera_root_position, rumble_intensity, rumble_intensity)
-		step_complete(0)
-	elif step == OutroStep.STATIONARY:
-		print("STATIONARY Ground Cam")
-		if rumble_intensity > 0:
-			rumble(ground_cam, camera_root_position, rumble_intensity, rumble_intensity)
-	elif step == OutroStep.INTERCEPT:
-		print("INTERCEPT Follow Cam")
-		rumble(follow_cam, camera_root_position, rumble_intensity, rumble_intensity)
-	elif step == OutroStep.SPACE:
-		print("SPACE Follow Cam")
-		rumble(follow_cam, camera_root_position, rumble_intensity, rumble_intensity)
-	elif step == OutroStep.SUN:
-		print("SUN Follow Cam")
-		rumble(follow_cam, camera_root_position, rumble_intensity, rumble_intensity)
-	else:
-		print("Impossible State")
-
-func step_complete(step_completed):
-	if step_completed == OutroStep.FOLLOW:
-		print("FOLLOW Cam Step Complete")
-		camera_root_position = ground_cam.position
-		ground_cam.make_current()
-		tween = Transition12Tweens.step2(self, get_tree().create_tween())
-		step = OutroStep.STATIONARY
-	elif step_completed == OutroStep.STATIONARY:
-		print("STATIONARY Cam Step Complete")
-		camera_root_position = follow_cam.position
-		follow_cam.make_current()
-		tween = Transition12Tweens.step3()
-		step = OutroStep.INTERCEPT
-	elif step_completed == OutroStep.INTERCEPT:
-		print("INTERCEPT Cam Step Complete")
-		tween = Transition12Tweens.step4()
-		step = OutroStep.SPACE
-	elif step_completed == OutroStep.SPACE:
-		print("SPACE Cam Step Complete")
-		tween = Transition12Tweens.step5()
-		step = OutroStep.STATIONARY
-	elif step_completed == OutroStep.SUN:
-		print("SUN Cam Step Complete")
-		tween = Transition12Tweens.step6()
-	else:
-		print("Impossible State")
-		
-func rumble(node, root_position, x_scale = 1, y_scale = 1):
-	var random_rumble_x = root_position.x + randf_range(0, MAX_RUMBLE_VARIANCE.x) * x_scale
-	var random_rumble_y = root_position.y + randf_range(0, MAX_RUMBLE_VARIANCE.y) * y_scale
-	node.position = Vector3(random_rumble_x, random_rumble_y, root_position.z)
+func _process(delta):
+	AnimationUtils.rumble(ground_cam, camera_root_position, rumble_intensity, rumble_intensity)
 	
 func finish():
 	PlayerStats.is_gameplay_running = true
@@ -91,8 +30,15 @@ func start_tween_animation(callback):
 	# Shake Camera as rocket flies by
 	tween.set_ease(Tween.EASE_IN)
 	tween.set_trans(Tween.TRANS_SINE)
-	tween.tween_property(self, "rumble_intensity", 4,  3)
+	tween.tween_property(self, "rumble_intensity", 8,  3)
+	
+	
 	tween.set_ease(Tween.EASE_OUT)
 	tween.set_trans(Tween.TRANS_SINE)
-	tween.tween_property(self, "rumble_intensity", 0,  5)	
+	tween.tween_property(self, "rumble_intensity", 0,  5)
+	
+	tween.set_ease(Tween.EASE_OUT)
+	tween.set_trans(Tween.TRANS_CUBIC)
+	tween.parallel().tween_property(player_origin, "position:z", 152,  5)
+	tween.parallel().tween_property(player_origin, "rotation_degrees:x", -150,  7).as_relative()
 	tween.tween_callback(callback)	
